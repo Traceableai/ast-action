1b9efca8c95fd7c17a6b81dd2c7d0183c8f2fb2a
Dhruv Singhal <dhruvsinghal@Dhruvs-MacBook-Pro.local>
2022-09-26 12:46:49 +0530

    added support to download rc cli

--- traceable-ast/downloadCli.sh
+++ traceable-ast/downloadCli.sh
@@ -2,9 +2,10 @@ if [[ ${1}  = *"-rc."* ]]
 then
   curl https://downloads.traceable.ai/\#cli/rc/${1}/tars/traceable-cli-${1}-linux-x86_64.tar.gz -o ${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64.tar.gz
   tar -xf ${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64.tar.gz --directory ${GITHUB_WORKSPACE}
-  echo "::set-env name=CLI_LOCATION::${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64"
+  export CLI_LOCATION=${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64
 fi
 if [[ ${1} = "latest" ]]
 then
   ls -la
 fi
+source .bashrc
