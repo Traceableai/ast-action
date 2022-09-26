d90b9d32837f244a815b1c75cbf5df89bf0ea609
Dhruv Singhal <dhruvsinghal@Dhruvs-MacBook-Pro.local>
2022-09-26 12:46:49 +0530

    added support to download rc cli

--- traceable-ast/downloadCli.sh
+++ traceable-ast/downloadCli.sh
@@ -5,9 +5,10 @@ then
 fi
 if [[ ${1} = "latest" ]]
 then
+  echo "entered latest"
   curl https://s3.amazonaws.com/downloads.traceable.ai\?list-type\=2\&delimiter\=%2F\&prefix\=cli%2Frc%2Flatest%2Ftars%2F -o latest.xml
   tar_file="$( grep -z -o  'traceable-cli.*linux-x86_64.tar.gz' latest.xml | sed 's/traceable-cli//' | grep -o 'traceable-cli.*' )"
   echo ${tar_file}
   curl -OL https://downloads.traceable.ai/cli/rc/latest/tars/${tar_file}
-  tar -xvf ${GITHUB_WORKSPACE}/${tar_file} --directory ${GITHUB_WORKSPACE}
+  tar -xvf ${tar_file}
 fi
