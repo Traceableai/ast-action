eb17c73763d1e1b131c722e5d7f88b0fcc6bd09a
Dhruv Singhal <dhruvsinghal@Dhruvs-MacBook-Pro.local>
2022-09-26 12:46:49 +0530

    added support to download rc cli

--- traceable-ast/downloadCli.sh
+++ traceable-ast/downloadCli.sh
@@ -1,12 +1,10 @@
 if [[ ${1}  = *"-rc."* ]]
 then
-  echo "reached in first if condition"
-  curl https://downloads.traceable.ai/\#cli/rc/${{ inputs.cli_version }}/tars/traceable-cli-${{ inputs.cli_version }}-linux-x86_64.tar.gz -o ${GITHUB_WORKSPACE}/traceable-cli-${{ inputs.cli_version }}-linux-x86_64.tar.gz
-  tar -xf ${GITHUB_WORKSPACE}/traceable-cli-${{ inputs.cli_version }}-linux-x86_64.tar.gz --directory ${GITHUB_WORKSPACE}
-  echo "::set-env name=CLI_LOCATION::${GITHUB_WORKSPACE}/traceable-cli-${{ inputs.cli_version }}-linux-x86_64"
+  curl https://downloads.traceable.ai/\#cli/rc/${1}/tars/traceable-cli-${1}-linux-x86_64.tar.gz -o ${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64.tar.gz
+  tar -xf ${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64.tar.gz --directory ${GITHUB_WORKSPACE}
+  echo "::set-env name=CLI_LOCATION::${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64"
 fi
 if [[ ${1} = "latest" ]]
 then
   ls -la
 fi
-echo "reached the end of run"
