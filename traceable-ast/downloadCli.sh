5706b1528318f11042e0db3db9bfa21174d4cbe2
Dhruv Singhal <dhruvsinghal@Dhruvs-MacBook-Pro.local>
2022-09-28 15:03:57 +0530

    added downloading latest cli

--- traceable-ast/downloadCli.sh
+++ traceable-ast/downloadCli.sh
@@ -7,7 +7,6 @@ if [[ ${1} = "latest" ]]
 then
   tar_file="$( curl -s https://s3.amazonaws.com/downloads.traceable.ai\?list-type=2\&delimiter\=%2F\&prefix=cli%2Frc%2Flatest%2Ftars%2F |  grep -z -o --text  'traceable-cli.*linux-x86_64.tar.gz' | sed 's/traceable-cli//' | grep -o -a 'traceable-cli.*'  )"
   echo ${tar_file}
-  echo "no tar file name"
   curl -OL https://downloads.traceable.ai/cli/rc/latest/tars/${tar_file}
   tar -xvf ${tar_file} --directory ${GITHUB_WORKSPACE}
 fi
