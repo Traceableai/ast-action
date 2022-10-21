fae29157be31f9c13764f462fc35f648c17f159a
Dhruv Singhal <dhruv.singhal@traceable.ai/>
2022-10-21 18:47:40 +0530

    shell  files dependency removal

--- downloadCli.sh
+++
@@ -1,9 +0,0 @@
-if [[ ${1}  = *"-rc."* ]]
-then
-  curl -OL https://downloads.traceable.ai/cli/rc/${1}/tars/traceable-cli-${1}-linux-x86_64.tar.gz
-fi
-if [[ ${1} = "latest" ]]
-then
-  curl -OL https://downloads.traceable.ai/cli/rc/${1}/traceable-cli-${1}-linux-x86_64.tar.gz
-fi
-tar -xvf ${GITHUB_WORKSPACE}/traceable-cli-${1}-linux-x86_64.tar.gz --directory ${GITHUB_WORKSPACE}
