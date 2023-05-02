4c3a7b0ed68477d72a1d40bfe3c2f5269209bec0
ankitchoudhary111 <35135474+ankitchoudhary111@users.noreply.github.com>
2023-05-02 18:52:34 +0530

    fix py script

--- generate_report.py
+++ generate_report.py
@@ -3,10 +3,9 @@ from junitparser import JUnitXml, TestCase, TestSuite, Properties
 if __name__ == '__main__':
     report = JUnitXml()
     testsuite = TestSuite("Scan Details")
-    testsuite.properties = Properties()
-    testsuite.properties.add_property('Name', "scan_name")
-    testsuite.properties.add_property('ID', "sdfg23456vbj678bn")
-    testsuite.properties.add_property('Env', "Prod")
+    testsuite.add_property('Name', "scan_name")
+    testsuite.add_property('ID', "sdfg23456vbj678bn")
+    testsuite.add_property('Env', "Prod")
     report.add_testsuite(testsuite)
     with open("report.xml", "w") as f:
         report.write(f)
